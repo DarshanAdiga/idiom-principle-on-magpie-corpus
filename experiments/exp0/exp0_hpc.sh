a317238e40739638490f50620ec8748ca8556f20
Darshan Adiga <3319126+DarshanAdiga@users.noreply.github.com>
2022-07-18 19:24:24 +0100

    Working GPU batch submission of exp0

--- experiments/exp0/exp0_hpc.sh
+++ experiments/exp0/exp0_hpc.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #$ -l h_rt=3:00:00  #time needed
-#$ -pe smp 2 #number of cores
+#$ -pe smp 3 #number of cores
 #$ -l rmem=4G #Maximum amount (xx) of real memory to be requested per CPU core
 #$ -l gpu=1 # Number of GPUs per every CPU core
-#$ -o ./exp_output.txt  #This is where your output and errors are logged.
+#$ -o ./output.txt  #This is where your output and errors are logged.
 #$ -j y # normal and error outputs into a single file (the file above)
 #$ -M dahaniyanarayana1@sheffield.ac.uk #Notify you by email, remove this line if you don't like
 #$ -m ea #Email you when it finished or aborted
