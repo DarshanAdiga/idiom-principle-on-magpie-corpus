a223d4c77016771a298518e2dda32a59bc06f2b3
Darshan Adiga <3319126+DarshanAdiga@users.noreply.github.com>
2022-07-18 19:14:03 +0100

    WIP: path updates

--- experiments/exp0/exp0.py
+++ experiments/exp0/exp0.py
@@ -1,5 +1,8 @@
+# To make the 'exp_helpers' module visible here
+import sys
+sys.path.append("../../exp_helpers")
 
-from exp_helpers import run_glue_f1_macro
+import run_glue_f1_macro
 
 exp_model = 'bert-base-cased'
 exp_seed = 26
@@ -19,19 +22,20 @@ args = f"""
 --per_device_train_batch_size 32 \
 --learning_rate 2e-5 \
 --num_train_epochs 6 \
---evaluation_strategy "epoch" \
 --output_dir {model_checkpoint_dir} \
 --seed {exp_seed} \
 --train_file      {train_csv} \
 --validation_file {dev_csv} \
 --test_file       {test_csv} \
 --test_metrics \
---evaluation_strategy "epoch" \
---save_strategy "epoch"  \
+--evaluation_strategy epoch \
+--save_strategy epoch  \
 --load_best_model_at_end \
---metric_for_best_model "f1" \
+--metric_for_best_model f1 \
 --save_total_limit 3
 """
+
 # NOTE: This is a bit of a hack, but works for now
 args = args.replace("\n", " ").split()
-run_glue_f1_macro.main(args)
+sys.argv = [sys.argv[0]] + args
+run_glue_f1_macro.main()
