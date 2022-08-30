044c07cf8d5a81d3aa838db3b71b37c2b9e1d208
Darshan Adiga <3319126+DarshanAdiga@users.noreply.github.com>
2022-08-30 18:03:34 +0100

    COMPLETED! Segregated classification reports for exp3A_1 and exp3B_1

--- exp_helpers/produce_test_results.py
+++ exp_helpers/produce_test_results.py
@@ -9,11 +9,11 @@ from sklearn.metrics import confusion_matrix
 TARGET_NAMES = ['0', '1']
 
 
-def save_overall_classification_report(df_merged, out_dir):
+def save_overall_classification_report(y_true, y_pred, out_dir, outfile_prefix):
 
     # Get confusion matrix and save it as well
     print('-'*15, 'Confusion Matrix', '-'*15)
-    confusion_report = confusion_matrix(df_merged['label'], df_merged['prediction'], labels = [0, 1])
+    confusion_report = confusion_matrix(y_true, y_pred, labels = [0, 1])
     act_labels =['actual_'+lb for lb in TARGET_NAMES]
     pred_labels =['pred_'+lb for lb in TARGET_NAMES]
     df_confusion_report = pd.DataFrame(confusion_report, index=act_labels, columns=pred_labels)
@@ -21,12 +21,13 @@ def save_overall_classification_report(df_merged, out_dir):
     print()
 
     print('-'*15, 'Classification Report', '-'*15)
-    report = classification_report(df_merged['label'], df_merged['prediction'], target_names=TARGET_NAMES)
+    report = classification_report(y_true, y_pred, target_names=TARGET_NAMES)
     print(report)
     print('-'*40)
 
     # Save the report to a file
-    with open(os.path.join(out_dir, 'overall_classification_report.txt'), 'w') as f:
+    report_file = outfile_prefix + '_classification_report.txt'
+    with open(os.path.join(out_dir, report_file), 'w') as f:
         # Save the confusion matrix
         f.write('Confusion Matrix:\n')
         f.write(str(df_confusion_report))
@@ -35,21 +36,64 @@ def save_overall_classification_report(df_merged, out_dir):
         # Save the classification report
         f.write(report)
         f.write('\n')
-    print(f"Classification report saved to {out_dir}/classification_report.txt")
+    print(f"Classification report saved to {report_file}")
 
 
-def get_segregated_reports(df_merged, out_dir):
+def insert_test_PIE_tokens(df_merged, magpie_token_set):
     """
-    TODO: Compute Classification Report for each of the three kinds of PIE segregations
+    Identify those PIE tokens that are present in the df_merged, and add them as a separate column
+    Return the updated dataframe
     """
-    pass
+    test_PIE_list = list()
+    for i,mrow in df_merged.iterrows():
+        test_sent = mrow['sentence_0']
+        matched_token = None
+        for token in magpie_token_set:
+            if token in test_sent:
+                matched_token = token
+                break
+        if matched_token:
+            test_PIE_list.append(matched_token)
+        else:
+            raise Exception("No PIE token found in the sentence!")
+    # Add the PIE tokens as a column
+    df_merged['test_PIE_tokens'] = test_PIE_list
+    return df_merged
 
 
-def main(pred_file, test_file, out_dir):
+def save_segregated_reports(df_merged, df_pie_segregation, out_dir):
+    """
+    Compute & Save the Classification Reports for each of the three kinds of PIE segregations
+    """
+    # Join the dataframes
+    df_merged_segregated = pd.merge(df_merged, df_pie_segregation, left_on='test_PIE_tokens', \
+        right_on='idiom_token', how='inner')
+    # print(df_merged_segregated.columns)
+
+    # ------------------------------------
+    #  Compute the classification reports
+
+    # 1. Degree of Idiomaticity
+    for degree in df_merged_segregated['degree_of_idiomaticity'].unique():
+        df_subset = df_merged_segregated[df_merged_segregated['degree_of_idiomaticity'] == degree]
+        save_overall_classification_report(df_subset['label'], df_subset['prediction'], out_dir, outfile_prefix='degree_of_idiomaticity_'+degree)
+
+    # 2. CCNews Rarity
+    for rarity in df_merged_segregated['ccnews_rarity'].unique():
+        df_subset = df_merged_segregated[df_merged_segregated['ccnews_rarity'] == rarity]
+        save_overall_classification_report(df_subset['label'], df_subset['prediction'], out_dir, outfile_prefix='ccnews_rarity_'+rarity)
+
+    # 3. Morphology Type
+    for morph_type in df_merged_segregated['morphology_type'].unique():
+        df_subset = df_merged_segregated[df_merged_segregated['morphology_type'] == morph_type]
+        save_overall_classification_report(df_subset['label'], df_subset['prediction'], out_dir, outfile_prefix='morphology_type_'+morph_type)
+
+
+def main(pred_file, test_file, pie_segregation_file, out_dir):
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
     else:
-        print("Output directory already exists. Exiting.")
+        print("ERROR: Output directory already exists!! Exiting.")
         sys.exit(1)
 
     # Should contain tab separated 'index	prediction' columns
@@ -62,8 +106,21 @@ def main(pred_file, test_file, out_dir):
     df_merged = pd.merge(df_test, df_preds, on='index')
     assert df_merged.shape[0] == df_test.shape[0] and df_merged.shape[0] == df_preds.shape[0], "Error: Merging dataframes seems inconsistent!"
 
+    # Load the PIE segregation dataframe
+    df_pie_segregation = pd.read_csv(pie_segregation_file)
+    # Remove unimportant columns
+    df_pie_segregation = df_pie_segregation[[ 'idiom_token', 'degree_of_idiomaticity', 'ccnews_rarity', 'morphology_type']]
+    # Remove duplicates rows, (that were there due to 'label' column)
+    df_pie_segregation.drop_duplicates(inplace=True)
+
     # Compute the classification report
-    save_overall_classification_report(df_merged, out_dir)
+    save_overall_classification_report(df_merged['label'], df_merged['prediction'], out_dir, outfile_prefix='overall')
+
+    # Compute the classification report for each of the three kinds of PIE segregations
+    magpie_token_set = set(df_pie_segregation['idiom_token'].unique())
+    # Identify the PIE tokens in the test sentences
+    df_merged = insert_test_PIE_tokens(df_merged, magpie_token_set)
+    save_segregated_reports(df_merged, df_pie_segregation, out_dir)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -71,12 +128,14 @@ if __name__ == "__main__":
         ## Should contain tab separated 'index \t prediction' columns.")
     parser.add_argument("--test_file", type=str, required=True, help="Path to file containing true labels file, should have 'label' column. \
         ## Should contain comma separated 'sentence_0,label' columns.")
+    parser.add_argument("--pie_segregation_file", type=str, required=True, help="Path to file containing PIE segregation CSV file.")
     parser.add_argument("--out_dir", type=str, required=True, help="Output directory to save the test results")
 
     args = parser.parse_args()
     
     pred_file = args.pred_file
     test_file = args.test_file
+    pie_segregation_file = args.pie_segregation_file
     out_dir = args.out_dir
 
-    main(pred_file, test_file, out_dir)
+    main(pred_file, test_file, pie_segregation_file, out_dir)
--- experiments/exp3B_1/generate_test_results.sh
+++ experiments/exp3B_1/generate_test_results.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 python ../../exp_helpers/produce_test_results.py --pred_file ./checkpoints_SeqClassifier/test_results_None.txt \
     --test_file ./tmp/test.csv \
+    --pie_segregation_file ../../data/PIE_segregation/pie_segregation.csv \
     --out_dir ./test_report
